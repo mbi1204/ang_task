c4e7d9dcb0fb1106f70121cb72f302cd8aebbdc6
Israel <Mendoza>
2019-02-01 11:18:47 -0600

    Update cliente.service.ts

--- app/service/cliente.service.ts
+++ app/service/cliente.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import { HttpClient , HttpHeaders } from '@angular/common/http';
+import {ctCliente} from '../modelos/ctCliente';
 
 
 
@@ -7,24 +8,82 @@ import { HttpClient , HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class ClienteService {
-
+    public _ctCliente:ctCliente;
+    public _ctClientes:Array<ctCliente> = []; 
+    
     constructor(public _http: HttpClient) {
     }
+
+     headers: HttpHeaders  = new HttpHeaders({
+        'AuthKey': '',
+        'AuthToken': '',
+        'Content-Type': 'application/json'
+     });
 
     /*
         getLista
         Notas: Devuelve el lsitado de los cliente
     */
 
-    getLista() {
+    getLista() {        
+
+         return this._http.get( '/taskService/ctCliente',  {observe: 'response' ,  headers: this.headers });
+    }
+
+    
+
+
+    /**
+     * Crear un registro en la base de Datos
+     */
+    crear(){
         const headers = new HttpHeaders({
             'AuthKey': '',
             'AuthToken': '',
             'Content-Type': 'application/json'
          });
 
-
-         return this._http.get( '/taskService/ctCliente',  {observe: 'response' ,  headers: headers });
     }
+
+    /**
+     * Modifica registro en la base de Batos
+     */
+
+    modificar(){
+        const headers = new HttpHeaders({
+            'AuthKey': '',
+            'AuthToken': '',
+            'Content-Type': 'application/json'
+         });
+
+    } 
+
+    /**
+     * Elimina registro en la base de Datos
+     */
+
+
+    eliminar(){
+        const headers = new HttpHeaders({
+            'AuthKey': '',
+            'AuthToken': '',
+            'Content-Type': 'application/json'
+         });
+
+    }
+
+    /**
+     * Trae Registro de la Base de Datos
+     */
+    getRegistro(){
+        const headers = new HttpHeaders({
+            'AuthKey': '',
+            'AuthToken': '',
+            'Content-Type': 'application/json'
+         });
+
+    }
+
+
 
 }
